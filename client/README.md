5f0933d1b86355cbde75365982cf40faf233667c
Ege Ergül <63500329+egeergul@users.noreply.github.com>
2023-01-23 01:34:36 +0300

    initial commit

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# The frontend side of ExDev
--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-//hi
+import express from "express";
+
+
+const app = express();
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+
+
