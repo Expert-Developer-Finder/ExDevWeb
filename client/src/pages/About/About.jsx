cac06f01ea2643dcb6cc24abb3914906173ceb81
Ege Ergül <63500329+egeergul@users.noreply.github.com>
2023-01-23 04:43:44 +0300

    about is done

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,9 +1,170 @@
-import React from 'react'
+import React from "react";
+import "./aboutStyle.css";
 
-const About = () => {
+export default function About() {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="body-container">
+        <div className="header">
+          <h1>Expert Developer Recommender</h1>
+          <p>CS 491/2 Porject</p>
+        </div>
+        <div className="about-project">
+          <h1>About the project</h1>
+          <p>
+            Our team aims to implement a tool that will fasten the process of
+            developers getting familiar with new code bases with a crystal clear
+            understanding. Expert Developer Finder is a Visual Studio Extension
+            adoptable by a wide range of users (from small private projects
+            companies to tech giant companies). The extension will provide a
+            list of experienced developers to the developers who are added to
+            the code repository recently (e.g. interns, junior developers,
+            etc.). New developers can contact the experts (with Zoom, Slack and
+            email) for asking help about the specific artifacts/ code parts
+            where they are confused or about the received errors. Our algorithm
+            will accurately detect the true experts of the selected code part by
+            investigating many artifacts. With our tool, new developers won’t
+            waste their time searching for the correct developers who can truly
+            help them and won’t cause further confusion about their problems.
+            Thus, we believe the values we offer are vital for companies and
+            developers.
+          </p>
+        </div>
 
-export default About
+        <div className="info-card-container">
+          <h1>Team Members</h1>
+
+          <div className="row">
+            <div className="info-card">
+              <img
+                src={require("../../assets/about/team-members/bora.jpeg")}
+                alt="A photo of Bora Altınok"
+              />
+              <div className="info-card-text">
+                <h2>Bora Altınok</h2>
+                <p>21902206</p>
+                <a href="mailto:boraaltinok26@gmail.com">
+                  <p>boraaltinok26@gmail.com</p>
+                </a>
+                <a href="https://github.com/boraaltinok" target="_blank">
+                  Visit Bora's GitHub Profile
+                </a>
+              </div>
+            </div>
+
+            <div className="info-card">
+              <img
+                src={require("../../assets/about/team-members/ege.jpg")}
+                alt="A photo of Ege Ergül"
+              />
+
+              <div className="info-card-text">
+                <h2>Ege Ergül</h2>
+                <p>21902240</p>
+                <a href="mailto:egeergull2001@gmail.com">
+                  <p>egeergull2001@gmail.com</p>
+                </a>
+                <a href="https://github.com/egeergul" target="_blank">
+                  Visit Ege's GitHub Profile
+                </a>
+              </div>
+            </div>
+
+            <div className="info-card">
+              <img
+                src={require("../../assets/about/team-members/ali.jpg")}
+                alt="A photo of Ali Eren Günaltılı"
+              />
+
+              <div className="info-card-text">
+                <h2>Ali Eren Günaltılı</h2>
+                <p>21801897</p>
+                <a href="mailto:eren.gunaltili@ug.bilkent.edu.tr">
+                  <p>eren.gunaltili@ug.bilkent.edu.tr</p>
+                </a>
+
+                <a href="https://github.com/alierengunaltili" target="_blank">
+                  Visit Ali Eren's GitHub Profile
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div class="info-card">
+              <img
+                src={require("../../assets/about/team-members/ceyda.jpg")}
+                alt="A photo of Ceyda Şahin"
+              />
+
+              <div class="info-card-text">
+                <h2>Ceyda Şahin</h2>
+                <p>21903448</p>
+                <a href="mailto:ceyda.sahin@ug.bilkent.edu.tr">
+                  <p>ceyda.sahin@ug.bilkent.edu.tr</p>
+                </a>
+
+                <a href="https://github.com/ceydas" target="_blank">
+                  Visit Ceyda's GitHub Profile
+                </a>
+              </div>
+            </div>
+
+            <div class="info-card">
+              <img
+                src={require("../../assets/about/team-members/tuna.png")}
+                alt="A photo of Tuna Dalbeler"
+              />
+
+              <div class="info-card-text">
+                <h2>Tuna Dalbeler</h2>
+                <p>21802539</p>
+                <a href="mailto:tuna.dalbeler@ug.bilkent.edu.tr">
+                  <p>tuna.dalbeler@ug.bilkent.edu.tr</p>
+                </a>
+
+                <a href="https://github.com/contactLost" target="_blank">
+                  Visit Tunas's GitHub Profile
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div class="info-card"  >
+              <img
+              style={{ objectFit:"contain"}}
+                src={require("../../assets/about/team-members/eray_tuzun.jpg")}
+                alt="A photo of Eray Tüzün"
+              />
+
+              <div class="info-card-text">
+                <h2>Eray Tüzün</h2>
+                <a href="mailto:eraytuzun@cs.bilkent.edu.tr">
+                  <p>eraytuzun@cs.bilkent.edu.tr</p>
+                </a>
+
+                <a href="https://www.eraytuzun.com/index.html" target="_blank">
+                  Visit Mr. Tüzün's Website
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div className="footer">
+        <div className="footer-body">
+          <div className="mails">
+            <h1>Reach to our members</h1>
+            <a href="mailto:boraaltinok26@gmail.com">Bora Altınok</a>
+            <a href="mailto:egeergull2001@gmail.com">Ege Ergül</a>
+            <a href="mailto:ceydasahin889@gmail.com">Ceyda Şahin</a>
+            <a href="mailto:alierengunaltili@gmail.com">Ali Eren Günaltılı</a>
+            <a href="mailto:tuna.dalbeler@ug.bilkent.edu.tr">Tuna Dabeler</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/Plans/Plans.jsx
+++ client/src/pages/Plans/Plans.jsx
@@ -6,24 +6,28 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 function createData(price, condition) {
-  return { price, condition};
+  return { price, condition };
 }
 
 const rows = [
   createData("Free", "if we get A+"),
   createData("1$", "if we get A"),
-  createData("100$", "if we get B+ or lower")
-
+  createData("100$", "if we get B+ or lower"),
 ];
 
 export default function Plans() {
   return (
-    <Container style={{ display:"flex", justifyContent:"center"}} >
-      <TableContainer style={{marginTop:100, maxWidth:500}}   component={Paper} elevation={6}>
-        <Table  aria-label="simple table">
+    <Container
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Typography style={{ marginTop: 50 }} variant="h2">
+        The Pricing
+      </Typography>
+      <TableContainer style={{marginTop:15, maxWidth: 500 }} component={Paper} elevation={6}>
+        <Table aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell>Price</TableCell>
@@ -41,7 +45,6 @@ export default function Plans() {
                 </TableCell>
                 <TableCell align="right">{row.condition}</TableCell>
                 <TableCell align="right">{row.fat}</TableCell>
-
               </TableRow>
             ))}
           </TableBody>
