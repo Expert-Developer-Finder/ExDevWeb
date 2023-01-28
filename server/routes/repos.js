87f7306fcb21ff3e0f89d6245abbcae6df2c7404
Bora Altınok <baltinok@stilus.com.tr>
2023-01-28 11:18:04 +0300

    add member, add repo owner, get repo, checkIfRepoExists functions are implemented

--- server/routes/repos.js
+++ server/routes/repos.js
@@ -1,10 +1,20 @@
-import express  from "express";
-import { createRepo } from "../controllers/repos.js";
-
+import express from "express";
+import {
+  addMember,
+  checkIfRepoExists,
+  createRepo,
+  getRepo,
+  addRepoOwner,
+} from "../controllers/repos.js";
 
 const repoRouter = express.Router();
 
 repoRouter.post("/create", createRepo);
+repoRouter.get("/check-if-exists", checkIfRepoExists);
 
+repoRouter.put("/add-member", addMember);
+
+repoRouter.put("/add-repo-owner", addRepoOwner);
+repoRouter.get("/:id", getRepo);
 
 export default repoRouter;
