c7b47dda2643ee528a4031cc46789824bff9acae
egeergul <egeergull2001@gmail.com>
2023-05-10 00:32:03 +0300

    front bağlıyoz

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL:"http://localhost:5000"});
+const API = axios.create({baseURL:"https://exdev-server.onrender.com/"});
 
 API.interceptors.request.use((req)=> {
     if(localStorage.getItem("profile") ){
